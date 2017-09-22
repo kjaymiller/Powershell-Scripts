e36aaf96676d4f57130ba34143dbeb2ec4a17f52
Jay Miller <kjaymiller@gmail.com>
2017-09-22 10:15:03 -0700

    rename and cleanup on term employees script

---
+++ removeTermedEmployeesFromGroups.ps1
@@ -0,0 +1,17 @@
+﻿$termed = get-aduser -Filter {Description -like "*termed*"}
+
+foreach ($entry in $termed) {
+    $membership = Get-adgroup -LDAPFilter "(member=$entry)"
+
+    if ($membership.count -ne 0){
+        $name = $entry.Name
+        log $name
+
+        foreach ($group in $membership) {
+            Remove-ADGroupMember $group $entry -Verbose -Confirm:$false
+            }
+          
+            
+        }
+
+        }
